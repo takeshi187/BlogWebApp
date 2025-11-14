0029aebd260a2fea5707b7902660fb9d3b10c1bb
takeshi187 <94477473+takeshi187@users.noreply.github.com>
2025-11-14 20:22:51 +0500

    add index nad blog pages, article view model, article controller

---
+++ BlogWebApp/Controllers/BlogController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlogWebApp.Controllers
+{
+    public class BlogController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ BlogWebApp/ViewModels/ArticleViewModel.cs
@@ -0,0 +1,20 @@
+﻿using BlogWebApp.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlogWebApp.ViewModels
+{
+    public class ArticleViewModel
+    {
+        [Required(ErrorMessage = "Заголовок обязателен.")]
+        [MaxLength(300, ErrorMessage = "Длина заголовка не должна превышать 300 символов.")]
+        public string Title { get; set; } = null!;
+
+        public string? Image { get; set; }
+
+        [Required(ErrorMessage = "Текст поста обязателен.")]
+        public string Content { get; set; } = null!;
+
+        [Required(ErrorMessage = "Жанр обязателен.")]
+        public Guid GenreId { get; set; }
+    }
+}

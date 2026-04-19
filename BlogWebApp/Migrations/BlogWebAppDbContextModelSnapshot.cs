1365ecdab165f30a219929568c70954dfc454d6a
takeshi187 <94477473+takeshi187@users.noreply.github.com>
2026-04-19 20:39:45 +0500

    Projects and packages updated to.NET10

--- BlogWebApp/Migrations/BlogWebAppDbContextModelSnapshot.cs
+++ BlogWebApp/Migrations/BlogWebAppDbContextModelSnapshot.cs
@@ -17,7 +17,7 @@ namespace BlogWebApp.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "8.0.20")
+                .HasAnnotation("ProductVersion", "10.0.6")
                 .HasAnnotation("Relational:MaxIdentifierLength", 63);
 
             NpgsqlModelBuilderExtensions.UseIdentityByDefaultColumns(modelBuilder);
